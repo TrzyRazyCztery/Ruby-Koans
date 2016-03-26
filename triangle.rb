1659cc227f56030da2afdf7c2a91ee9190b94d35
TrzyRazyCztery <3x4to9@gmail.com>
2016-03-26 12:41:03 +0100

    about_triangle - done!

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if ( a == b or b == c or a == c)
+  	((a == b) and (b == c)) ? :equilateral : :isosceles
+  else
+  	:scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
